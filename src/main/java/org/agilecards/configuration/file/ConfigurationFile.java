0571ae38c8a531581b714b57b3975db814503cea
Wilcox Benoit <wilcox.benoit@gmail.com>
2017-03-17 23:11:23 +0000

    Load configuration

---
+++ src/main/java/org/agilecards/configuration/file/ConfigurationFile.java
@@ -0,0 +1,18 @@
+package org.agilecards.configuration.file;
+
+import java.util.List;
+
+public class ConfigurationFile {
+    
+    private String version;
+    private List<GeneratorConfig> generators;
+    
+    public String getVersion() {
+        return this.version;
+    }
+    
+    public GeneratorConfig getGeneratorConfig(String name) {
+        this.generators. 
+    }
+        
+}
