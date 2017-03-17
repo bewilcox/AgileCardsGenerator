b970394f92626b65beccfe94611a50f9e3c70104
Wilcox Benoit <wilcox.benoit@gmail.com>
2017-03-17 21:53:23 +0000

    Test for configuration

--- src/main/java/org/agilecards/configuration/AgileCardsConfiguration.java
+++ src/main/java/org/agilecards/configuration/AgileCardsConfiguration.java
@@ -12,20 +12,25 @@ public class AgileCardsConfiguration {
     //Logs
     private static final Logger LOG = LoggerFactory.getLogger(AgileCardsConfiguration.class);
 
-    // Constants
+    // Constants for CLI
     public static final String APP_NAME ="agile-cards";
-
     public static final String GENERATE_ACTION = "generate";
     public static final String GENERATE_USAGE = "Generate the agile cards";
-
     public static final String BLANK_ACTION = "blank";
     public static final String BLANK_USAGE = "Generate blank cards";
-
     public static final String INIT_ACTION = "init";
     public static final String INIT_USAGE = "Init the configuration and template files";
-
     public static final String VERSION_ACTION = "version";
     public static final String VERSION_USAGE = "Show the application version";
+
+    // Constants for Configuration File
+    public static final String PROP_DATA_FILE_PATH ="data_file";
+    public static final String PROP_HEADER ="header";
+    public static final String PROP_SHEET_NAME ="sheet_name";
+    public static final String PROP_JAZZ_SERVER_URL ="jazz_server_url";
+    public static final String PROP_JAZZ_LOGIN ="jazz_login";
+    public static final String PROP_JAZZ_PROJECT_AREA ="project_area";
+    public static final String PROP_JAZZ_QUERY_NAME ="query_name";
 
     // Others
     private String applicationVersion;
